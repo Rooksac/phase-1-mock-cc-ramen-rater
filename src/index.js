2b4d0d840632333a344a6c32f318ab3de4f4e94e
Zachary Dean <Zakedoubled@gmail.com>
2022-07-29 22:06:36 -0400

    ramen rater with POST

--- src/index.js
+++ src/index.js
@@ -3,35 +3,43 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         makeNewRamen()})
-
+    // editForm.addEventListener('submit', (e) => {
+    //     e.preventDefault()
+    //     editRamen()
+    // })
 })
 
 
 const ramenMenu = document.getElementById(`ramen-menu`)
 const ramenDetail = document.getElementById("ramen-detail")
 const namelist = document.querySelector('h2')
-const restaraunt = document.getElementById('restaurant')
+const restaurant = document.getElementById('restaurant')
 const ratingList = document.getElementById('rating-display')
 const commentList = document.getElementById('comment-display')
 const ramenDisplay = document.getElementById('ramen-display')
 const form = document.getElementById('new-ramen')
 const newName = document.getElementById('new-name')
-const newRestaraunt = document.getElementById('new-restaurant')
+const newRestaurant = document.getElementById('new-restaurant')
 const newComment = document.getElementById('new-comment')
 const newRating = document.getElementById('new-rating')
 const newImage = document.getElementById('new-image')
+const editForm = document.getElementById('edit-ramen')
+
+
 function getRamen() {
 fetch('http://localhost:3000/ramens')
 .then(res => res.json())
-.then(data => data.forEach(renderRamen))
+.then(data => {data.forEach(renderRamen)
+        onLoad(data[0])
+    })
 }
 
 function renderRamen(ramen) {
     let img = document.createElement('img')
     img.src = ramen.image
-    img.addEventListener('mouseover', () => {
+    img.addEventListener('click', () => {
         ramenDisplay.src = img.src, 
-        restaraunt.innerText = ramen.restaurant
+        restaurant.innerText = ramen.restaurant
         commentList.innerText = ramen.comment
         ratingList.innerText = `${ramen.rating}`
         namelist.innerText = ramen.name
@@ -44,7 +52,42 @@ function makeNewRamen() {
         image: `${newImage.value}`,
         comment: `${newComment.value}`,
         rating: `${newRating.value}`,
-        restaurant: `${newRestaraunt.value}`
+        restaurant: `${newRestaurant.value}`
     }
     renderRamen(newRamenObj);
+    postNewRamen(newRamenObj);
+    form.reset();
 }
+function onLoad(ramen) {
+    ramenDisplay.src = ramen.image
+    namelist.innerText = ramen.name
+    restaurant.innerText = ramen.restaurant
+    ratingList.innerText = ramen.rating
+    commentList.innerText = ramen.comment
+ 
+
+}
+// function editRamen(ramen) {
+//     fetch(`http://localhost:3000/ramens${ramen.id}` {
+//         method: 'PATCH',
+//         headers: {
+//             'content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(ramen)
+//     })
+//     .then(res => res.json())
+//     .then()
+    
+function postNewRamen(ramenObject) {
+    console.log(ramenObject);
+    fetch('http://localhost:3000/ramens', {
+        method:'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(ramenObject)
+        })
+        .then(res => res.json())
+
+    }
